5abc2dd4f2daef633863cc0a35908cd9449cef82
STHEFAN� BERECHAVINSKI VIEIRA <sthefany.1921@aluno.pr.senac.br>
2023-09-11 11:10:22 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/repositorio_teste/Repositorio_teste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositorio_teste;
+
+/**
+ *
+ * @author sthefany.1921
+ */
+public class Repositorio_teste {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
